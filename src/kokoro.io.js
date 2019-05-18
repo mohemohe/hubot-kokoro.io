c00f50e9d30b469596fc904dd1a8c3dfa0539216
mohemohe <mohemohe@ghippos.net>
2019-05-19 01:18:19 +0900

    WIP

--- src/kokoro.io.js
+++ src/kokoro.io.js
@@ -1,3 +1,5 @@
+//@ts-check
+
 /*
  * Description:
  *   Adapter for Hubot to communicate on kokoro.io
@@ -6,8 +8,9 @@
  *   None
  *
  * Configuration:
+ *   HUBOT_KOKOROIO_AS_USER        - user / bot, default: false
  *   HUBOT_KOKOROIO_ACCESSTOKEN    - access token
- *   HUBOT_KOKOROIO_CALLBACKSECRET - callback secret
+ *   HUBOT_KOKOROIO_CALLBACKSECRET - callback secret, only use in bot mode
  *
  * Notes:
  *
@@ -22,7 +25,11 @@ class KokoroIo extends Adapter {
     return new Promise(async (resolve) => {
       for (const string of strings) {
         this.robot.logger.info(`onSend ${envelope.room}: '${string}'`);
-        await this.kokoroIo.Api.Bot.postChannelMessage(envelope.room, { message: string });
+        if (this.asUser) {
+          await this.client.Api.Channels.postChannelMessage(envelope.room, { message: string });
+        } else {
+          await this.client.Api.Bot.postChannelMessage(envelope.room, { message: string });
+        }
       }
       resolve();
     });
@@ -33,7 +40,7 @@ class KokoroIo extends Adapter {
       let message = "";
       const h = /^(#+)\s.*$/i;
       if(string.match(h)) {
-        hs = h.exec(string)[1];
+        const hs = h.exec(string)[1];
         message = `${hs} @${envelope.message.screen_name} ${string.slice(hs.length+1)}`;
       } else {
         message = `@${envelope.message.screen_name} ${string}`;
@@ -58,29 +65,41 @@ class KokoroIo extends Adapter {
     this.receive(textMessage);
   }
 
+  get asUser() {
+    return process.env.HUBOT_KOKOROIO_AS_USER || false;
+  }
+
   run() {
+    String.raw`
+    ___  __    ________  ___  __    ________  ________  ________      ___  ________
+    |\  \|\  \ |\   __  \|\  \|\  \ |\   __  \|\   __  \|\   __  \    |\  \|\   __  \
+    \ \  \/  /|\ \  \|\  \ \  \/  /|\ \  \|\  \ \  \|\  \ \  \|\  \   \ \  \ \  \|\  \
+     \ \   ___  \ \  \\\  \ \   ___  \ \  \\\  \ \   _  _\ \  \\\  \   \ \  \ \  \\\  \
+      \ \  \\ \  \ \  \\\  \ \  \\ \  \ \  \\\  \ \  \\  \\ \  \\\  \ __\ \  \ \  \\\  \
+       \ \__\\ \__\ \_______\ \__\\ \__\ \_______\ \__\\ _\\ \_______\\__\ \__\ \_______\
+        \|__| \|__|\|_______|\|__| \|__|\|_______|\|__|\|__|\|_______\|__|\|__|\|_______|
+    `.split('\n').forEach((line) => this.robot.logger.info(line));
+
+    if (this.asUser) {
+      this.runAsUser();
+    } else {
+      this.runAsBot();
+    }
+  }
+
+  runAsBot() {
     if(!process.env.HUBOT_KOKOROIO_ACCESSTOKEN || !process.env.HUBOT_KOKOROIO_CALLBACKSECRET) {
       this.robot.logger.error("Error: 'HUBOT_KOKOROIO_ACCESSTOKEN' and 'HUBOT_KOKOROIO_CALLBACKSECRET' are required");
       throw new Error();
     }
 
-    String.raw`
-___  __    ________  ___  __    ________  ________  ________      ___  ________
-|\  \|\  \ |\   __  \|\  \|\  \ |\   __  \|\   __  \|\   __  \    |\  \|\   __  \
-\ \  \/  /|\ \  \|\  \ \  \/  /|\ \  \|\  \ \  \|\  \ \  \|\  \   \ \  \ \  \|\  \
- \ \   ___  \ \  \\\  \ \   ___  \ \  \\\  \ \   _  _\ \  \\\  \   \ \  \ \  \\\  \
-  \ \  \\ \  \ \  \\\  \ \  \\ \  \ \  \\\  \ \  \\  \\ \  \\\  \ __\ \  \ \  \\\  \
-   \ \__\\ \__\ \_______\ \__\\ \__\ \_______\ \__\\ _\\ \_______\\__\ \__\ \_______\
-    \|__| \|__|\|_______|\|__| \|__|\|_______|\|__|\|__|\|_______\|__|\|__|\|_______|
-`.split('\n').forEach((line) => this.robot.logger.info(line));
-
     if (!this.robot.name.match(/^@.*$/)) {
       this.robot.name = "@${this.robot.name}"
     }
 
-    this.token = process.env.HUBOT_KOKOROIO_ACCESSTOKEN || ''
-    this.secret = process.env.HUBOT_KOKOROIO_CALLBACKSECRET || ''
-    this.kokoroIo = new kokoro.io({
+    this.token = process.env.HUBOT_KOKOROIO_ACCESSTOKEN || '';
+    this.secret = process.env.HUBOT_KOKOROIO_CALLBACKSECRET || '';
+    this.client = new kokoro.io({
       accessToken: this.token
     });
 
@@ -100,7 +119,39 @@ ___  __    ________  ___  __    ________  ________  ________      ___  ________
     this.emit('connected');
   }
 
+  runAsUser() {
+    if(!process.env.HUBOT_KOKOROIO_ACCESSTOKEN) {
+      this.robot.logger.error("Error: 'HUBOT_KOKOROIO_ACCESSTOKEN' is required");
+      throw new Error();
+    }
 
+    if (!this.robot.name.match(/^@.*$/)) {
+      this.robot.name = "@${this.robot.name}"
+    }
+
+    this.token = process.env.HUBOT_KOKOROIO_ACCESSTOKEN || '';
+    this.client = new kokoro.io({
+      accessToken: this.token,
+      autoReconnect: true,
+    });
+
+    this.client.Stream.on(this.client.Stream.Event.Connect, async () => {
+      const memberships = await this.client.Api.Memberships.getMemberships();
+      const channelIds = this.client.Helper.membershipsToChannelIds(memberships);
+      this.client.Helper.subscribeChatChannelByChannelIds(this.client.Stream, channelIds);
+
+      this.emit('connected');
+    });
+
+    this.client.Stream.on(this.client.Stream.Event.Chat, (message) => {
+      if (message.event === this.client.Stream.PuriparaEvent.MessageCreated) {
+        this.chat(message.data);
+      }
+      console.log(message)
+    });
+
+    this.client.Stream.connect(true);
+  }
 }
 
 exports.use = (robot) => new KokoroIo(robot);
